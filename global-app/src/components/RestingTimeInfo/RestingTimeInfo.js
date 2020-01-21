295dd8f976fef99c2787a507fe62c48bc86445f3
evan-boissonnot <commercial@boissonnot.Fr>
2020-01-21 23:05:15 +0100

    evo: adding smiley

--- global-app/src/components/RestingTimeInfo/RestingTimeInfo.js
+++ global-app/src/components/RestingTimeInfo/RestingTimeInfo.js
@@ -2,13 +2,14 @@ import React  from 'react';
 import { observer } from "mobx-react";
 
 import './resting-time-info.css';
+import Smiley from '../Smiley/Smiley';
 
 const RestingTimeInfo = observer((props) => {
     const store = props.store;
 
     return (
         <div className="resting-time-info">
-            
+            <Smiley></Smiley>
         </div>
     )
 });
---
+++ global-app/src/components/Smiley/Smiley.js
@@ -0,0 +1,14 @@
+import React  from 'react';
+import './smiley.css';
+
+const Smiley = (props) => {
+    return (
+        <div className="smiley-green floatleft">
+            <div className="left-eye"></div>
+            <div className="right-eye"></div>
+            <div className="smile"></div>
+        </div>
+        );
+    }
+    
+    export default Smiley;
